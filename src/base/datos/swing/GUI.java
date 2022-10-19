22f3735b8390bd8e945013f3b104cca8edfcd9b1
Marco <marcomph@hotmail.com>
2022-10-19 11:16:39 -0500

    Botón de crear base de datos quitado

--- src/base/datos/swing/GUI.java
+++ src/base/datos/swing/GUI.java
@@ -156,7 +156,6 @@ public class GUI extends javax.swing.JFrame {
         jTextFieldMatricula = new javax.swing.JTextField();
         jTextFieldCarrera = new javax.swing.JTextField();
         jLabel20 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jTextFieldUID2 = new javax.swing.JTextField();
         jLabel14 = new javax.swing.JLabel();
@@ -381,13 +380,6 @@ public class GUI extends javax.swing.JFrame {
 
         jLabel20.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/itesm.jpg"))); // NOI18N
 
-        jButton2.setText("Crear nueva tabla");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
@@ -421,13 +413,11 @@ public class GUI extends javax.swing.JFrame {
                                         .addComponent(jTextFieldUID, javax.swing.GroupLayout.PREFERRED_SIZE, 234, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                 .addGap(0, 0, Short.MAX_VALUE))))
                     .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jButtonEnviarSQL, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(jButtonEnviarSQL)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel20, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(258, Short.MAX_VALUE))
+                .addContainerGap(259, Short.MAX_VALUE))
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addComponent(jLabel13)
                 .addGap(0, 401, Short.MAX_VALUE))
@@ -466,9 +456,7 @@ public class GUI extends javax.swing.JFrame {
                             .addComponent(jTextFieldCarrera, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel11))
                         .addGap(18, 18, 18)
-                        .addComponent(jButtonEnviarSQL)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jButton2))
+                        .addComponent(jButtonEnviarSQL))
                     .addComponent(jLabel20))
                 .addGap(0, 225, Short.MAX_VALUE))
         );
@@ -830,11 +818,6 @@ int bank, address, count;
         // TODO add your handling code here:
     }//GEN-LAST:event_jTabbedPane1StateChanged
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        // Boton crear base datos
-    }//GEN-LAST:event_jButton2ActionPerformed
-
     /**
      * @param args the command line arguments
      */
@@ -883,7 +866,6 @@ int bank, address, count;
     private javax.swing.JButton ConnectReaderButton;
     private javax.swing.JTextField SerialPortTextField;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton9;
